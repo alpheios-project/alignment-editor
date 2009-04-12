16b069345624a0127366d98435cf295ba59b8ed2
MichaelGursky <MichaelGursky@6cdfca17-2474-42dc-bd0f-2d6b4f5baa86>
2009-04-12 03:44:45 +0000

    Add display of marks and highlighting of sentences with marked words in alignment sentence list. Add missing boilerplate headers.
    git-svn-id: https://svn.code.sf.net/p/alpheios/code/utilities/alignment-editor/trunk@1055 6cdfca17-2474-42dc-bd0f-2d6b4f5baa86

--- alph-align-edit.js
+++ alph-align-edit.js
@@ -1,3 +1,25 @@
+/**
+ * @fileoverview alph-align-edit - alignment editor
+ *  
+ * Copyright 2009 Cantus Foundation
+ * http://alpheios.net
+ * 
+ * This file is part of Alpheios.
+ * 
+ * Alpheios is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Alpheios is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 var s_ns = "http://www.w3.org/2000/svg";        // svg namespace
 var s_xlinkns = "http://www.w3.org/1999/xlink"; // xlink namespace
 var s_fontSize = 20;                            // regular font size
---
+++ alph-align-list.js
@@ -0,0 +1,41 @@
+/**
+ * @fileoverview alph-align-list - alignment sentence list
+ *  
+ * Copyright 2009 Cantus Foundation
+ * http://alpheios.net
+ * 
+ * This file is part of Alpheios.
+ * 
+ * Alpheios is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Alpheios is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+s
+//****************************************************************************
+// marks
+//****************************************************************************
+
+// mouse over element with marks
+function ShowMarks(a_elt)
+{
+  var tip = a_elt.getElementsByClassName("alph-marks")[0];
+  tip.style.position = "relative";
+  tip.style.visibility = "visible";
+};
+
+// mouse out of element with marks
+function HideMarks(a_elt)
+{
+  var tip = a_elt.getElementsByClassName("alph-marks")[0];
+  tip.style.position = "absolute";
+  tip.style.visibility = "hidden";
+};
